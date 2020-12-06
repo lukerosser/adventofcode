5fd26531d0faef386c53409b7b61fb239b43f9ed
Luke Rosser <luke.rosser@kpmg.co.uk>
2020-12-06 19:46:47 +0000

    Typing and clearer function params

--- 2020/day-6/customs.py
+++ 2020/day-6/customs.py
@@ -6,14 +6,14 @@ answers1 = ""
 answers2 = ""
 group_members = 0
 
-def declarations(group_answers, count):
-  if count == 0:
+def declarations(group_answers: str, number_of_people: int = None) -> int:
+  if not number_of_people:
     return len(set(group_answers))
   else:
     common_answers = []
     unique_answers = set(group_answers)
     for answer in unique_answers:
-      if group_answers.count(answer) == count:
+      if group_answers.count(answer) == number_of_people:
         common_answers.append(answer)
     return len(common_answers)
 
@@ -23,13 +23,13 @@ for line in input:
     answers2 = answers2 + line.strip("\n")
     group_members += 1
   else:
-    answer_counts.append(declarations(answers1, 0))
+    answer_counts.append(declarations(answers1))
     same_answer_counts.append(declarations(answers2, group_members))
     answers1 = ""
     answers2 = ""
     group_members = 0
 
-answer_counts.append(declarations(answers1, 0))
+answer_counts.append(declarations(answers1))
 same_answer_counts.append(declarations(answers2, group_members))
 
 print(f"Answer 1: {sum(answer_counts)}")
